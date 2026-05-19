3c8760afaf9dab0bb05238505ef62dda3353275b
fgiust <fgiust@gmail.com>
2026-05-19 07:32:11 +0200

    fix vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ import FamilyNamesPage from './pages/FamilyNamesPage';
 import DisclaimerPage from './pages/DisclaimerPage';
 import AdjectivesNounsPage from './pages/AdjectivesNounsPage';
 import FeedbackPanel from './components/FeedbackPanel';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from '@vercel/analytics/react';
 
 function DarkModeToggle() {
   const [dark, setDark] = useState(() => {
