2c24cbf75e6199484df914ca42158657cce24bf9
fgiust <fgiust@gmail.com>
2026-05-20 09:32:08 +0200

    cleanup

--- test-regex2.mjs
+++
@@ -1,6 +0,0 @@
-const txt = `      { english: 'How much is that over there?', answer: 'あれは{いくら|なん円[えん]}{ですか|か}' },`;
-const regex = /{\s*english:\s*(['"`])([\s\S]*?)\1,\s*answer:\s*(['"`])([\s\S]*?)\3\s*}/g;
-let m;
-while(m = regex.exec(txt)) {
-  console.log(m[2]);
-}
--- test-trans.mjs
+++
@@ -1,7 +0,0 @@
-import translate from 'google-translate-api-x';
-
-async function test() {
-  const res = await translate('Hello world', { to: 'it' });
-  console.log(res.text);
-}
-test();
