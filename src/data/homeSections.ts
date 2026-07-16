28b40edb35cfb90bf9cb9a02e56242fadffcd2be
fgiust <fgiust@gmail.com>
2026-07-16 22:12:09 +0200

    content review

--- src/data/homeSections.ts
+++ src/data/homeSections.ts
@@ -383,7 +383,7 @@ export const homeConfig: HomeConfig = {
         { id: 'genki18-1' },
         { id: 'genki18-2' },
         { id: 'genki18-3' },
-        { id: 'genki18-4', beta: true },
+        { id: 'genki18-4' },
         { id: 'genki18-5', beta: true },
         { id: 'genki18-6', beta: true },
       ],
